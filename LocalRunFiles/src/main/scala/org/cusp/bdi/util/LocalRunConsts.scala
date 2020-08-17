6fe9216770a0b32459c4527c3287d38212fbf279
ayman <>
2020-08-17 00:40:30 -0400

    1. added region expansion by 2 * sqrt(2 * ((w-1)/w)^2) 2. Scans leftRDD to compute # of rounds

--- LocalRunFiles/src/main/scala/org/cusp/bdi/util/LocalRunConsts.scala
+++ LocalRunFiles/src/main/scala/org/cusp/bdi/util/LocalRunConsts.scala
@@ -45,9 +45,9 @@ object LocalRunConsts {
   val pathKM_Bus_WGS84 = "/media/cusp/Data/GeoMatch_Files/InputFiles/KM_WGS84_LION_LineString_Bus_Point_ErrCorr_MBRExp_150_MaxDist_150.1K.csv"
   val pathKM_RandomPointsNonUniform = ""
 
-  val pathGM_TPEP = "/media/cusp/Data/GeoMatch_Files/OutputFiles/Simba/Taxi_1_Grp1/"
+  val pathGM_TPEP = "/media/cusp/Data/GeoMatch_Files/OutputFiles/Simba/TLC_3_Grp2/"
   val pathSparkKNN_FW_Output_1 = pathGM_TPEP
-  val pathSparkKNN_FW_Output_2 = "/media/cusp/Data/GeoMatch_Files/OutputFiles/40/"
+  val pathSparkKNN_FW_Output_2 = "/media/cusp/Data/GeoMatch_Files/OutputFiles/528/"
 
   val pathGS_TPEP = "/media/cusp/Data/GeoMatch_Files/InputFiles/GeoSpark_LION_TPEP.csv"
   val pathLS_wgs_TPEP = "/media/cusp/Data/GeoMatch_Files/InputFiles/LocationSpark_LION_TPEP.csv"
@@ -58,8 +58,8 @@ object LocalRunConsts {
 
   val pathTaxi1M_NAD83 = "/media/cusp/Data/GeoMatch_Files/InputFiles/Yellow_TLC_TripRecord_NAD83_1M.csv"
 
-  val pathRandSample_A_NAD83 = "/media/cusp/Data/GeoMatch_Files/InputFiles/RandomSamples_OLD/Taxi_1_A.csv"
-  val pathRandSample_B_NAD83 = "/media/cusp/Data/GeoMatch_Files/InputFiles/RandomSamples_OLD/Taxi_1_B.csv"
+  val pathRandSample_A_NAD83 = "/media/cusp/Data/GeoMatch_Files/InputFiles/RandomSamples/Bread_3_A.csv"
+  val pathRandSample_B_NAD83 = "/media/cusp/Data/GeoMatch_Files/InputFiles/RandomSamples/Bread_3_B.csv"
 
   //  val pathTaxi1M_No_Trip_NAD83_A = "/media/cusp/Data/GeoMatch_Files/InputFiles/Yellow_TLC_TripRecord_NAD83_1M_No_Trip_Info_A.csv"
   //  val pathTaxi1M_No_Trip_NAD83_B = "/media/cusp/Data/GeoMatch_Files/InputFiles/Yellow_TLC_TripRecord_NAD83_1M_No_Trip_Info_B.csv"
--- Spark_kNN/src/main/scala/org/cusp/bdi/sknn/util/GridOperation.scala
+++ Spark_kNN/src/main/scala/org/cusp/bdi/sknn/util/GridOperation.scala
@@ -1,12 +1,16 @@
 package org.cusp.bdi.sknn.util
 
-class GridOperation(mbr: (Double, Double, Double, Double), totalRowCount: Long, k: Int) extends Serializable {
+case class GridOperation(mbr: (Double, Double, Double, Double), totalRowCount: Long, k: Int) extends Serializable {
 
   private val pointPerBox = math.ceil(totalRowCount.toDouble / k).toLong
 
   private val boxWH = math.max(math.ceil((mbr._4 - mbr._2) / pointPerBox), math.ceil((mbr._3 - mbr._1) / pointPerBox))
 
+  private val shiftErrorRange = 2 * math.sqrt((boxWH - 1) / boxWH)
+
   def getBoxWH = boxWH
+
+  def getShiftErrorRange = shiftErrorRange
 
   def computeBoxXY(xy: (Double, Double)): (Long, Long) =
     computeBoxXY(xy._1, xy._2)
@@ -18,11 +22,11 @@ class GridOperation(mbr: (Double, Double, Double, Double), totalRowCount: Long, 
     (math.floor(scaled._1).toLong, math.floor(scaled._2).toLong)
   }
 
-  def scaleXY(xy: (Double, Double)): (Double, Double) =
-    scaleXY(xy._1, xy._2)
-
   def scaleXY(x: Double, y: Double) =
     (x / boxWH, y / boxWH)
+
+  def scaleXY(xy: (Double, Double)): (Double, Double) =
+    scaleXY(xy._1, xy._2)
 
   //    (math.round(x / boxWH), math.round(y / boxWH))
 }
